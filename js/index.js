f6169771047fe81254125260cf6578a52bb58520
PeterArce96 <parce0496@gmail.com>
2023-02-22 18:54:20 -0500

    calculadora - finalización, correciones

--- js/index.js
+++ js/index.js
@@ -16,13 +16,11 @@ const documentReady = () => {
         
         if (isNaN(numero1) || isNaN(numero2)) {
             mostrarError('Completar el formulario')
-            setTimeout(() => {
-                location.reload();
-            }, 3000);
+
             
         } else {
             let respuesta = 0;
-            error.innerHTML = '';
+            // error.innerHTML = '';
             switch (operador) {
                 case '+':
                     respuesta = (numero1 + numero2);
@@ -36,19 +34,19 @@ const documentReady = () => {
                 case '/':
                     if (numero1 === 0 || numero2 === 0) {
                         mostrarError('Los operadores no pueden ser cero')
-                        setTimeout(() => {
-                            location.reload();
-                        }, 3000);
+
                     } else {
                         respuesta = (numero1 / numero2);
                     }    
                     break;
 
                 default:
-                    error.innerHTML = 'Seleccionar una operación válida'
+                    mostrarError('Seleccionar una operación válida')
                     break;
             }
-            resultado.innerHTML = 'Resultado: ' + respuesta;
+            
+            resultado.innerHTML = 'Resultado: ' + Intl.NumberFormat('de-DE').format(respuesta);
+            resultado.classList.add('resultado')
         }
 
     } 
@@ -58,8 +56,10 @@ const documentReady = () => {
     function mostrarError(mensaje) {
         const mensajeError = document.querySelector('#error');
         mensajeError.textContent = mensaje;   
-        mensajeError.style.display = 'block'
-
+        mensajeError.classList.add('error')
+        setTimeout(() => {
+            mensajeError.remove();
+        }, 3000);
         
     }
 
